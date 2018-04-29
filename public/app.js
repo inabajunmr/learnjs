1b910249b92a54e423b9a7982fdce6ba06737f55
inabajunmr <inabajun.for.regi@gmail.com>
2018-04-29 22:13:52 +0900

    p140

--- public/app.js
+++ public/app.js
@@ -16,7 +16,6 @@ learnjs.problems = [
     }
 ]
 
-
 learnjs.problemView = function(data) {
   var problemNumber = parseInt(data, 10);
   var view = $('.templates .problem-view').clone();
@@ -185,7 +184,7 @@ learnjs.profileView = function() {
     return view;
 }
 
-learnjs.sendDbRequest = function(req, retry){
+learnjs.sendAwsRequest = function(req, retry){
     var promise = new $.Deferred();
     req.on('error', function(error) {
         if(error.code === "CredentialsError") {
@@ -219,7 +218,7 @@ learnjs.saveAnswer = function(problemId, answer) {
                 answer : answer
             }
         };
-        return learnjs.sendDbRequest(db.put(item), function() {
+        return learnjs.sendAwsRequest(db.put(item), function() {
             return learnjs.saveAnswer(problemId, answer);
         })
     });
@@ -235,7 +234,7 @@ learnjs.fetchAnswer = function(problemId) {
                 problemId: problemId
             }
         };
-        return learnjs.sendDbRequest(db.get(item), function() {
+        return learnjs.sendAwsRequest(db.get(item), function() {
             return learnjs.fetchAnswer(problemId);
         })
     });
@@ -250,8 +249,21 @@ learnjs.countAnswers = function(problemId) {
             FilterExpression: 'problemId  = :problemId',
             ExpressionAttributeValues: {':problemId':problemId}
         };
-        return learnjs.sendDbRequest(db.scan(params), function() {
+        return learnjs.sendAwsRequest(db.scan(params), function() {
             return learnjs.countAnswers(problemId);
+        })
+    })
+}
+
+learnjs.poplarAnswers = function(problemId) {
+    return learnjs.identity.then(function() {
+        var lambda = new AWS.Lambda();
+        var params = {
+            FunctionName: 'learnjs_popularAnswers',
+            Payload: JSON.stringify({problemNumber: problemId})
+        };
+        return learnjs.sendAwsRequest(lambda.invoke(params), function(){
+            return learnjs.poplarAnswers(problemId);
         })
     })
 }
